06f8c449c1843e6475ff2e234e781ad242df533a
lexcorp <lr.romanov1999@mail.ru>
2020-09-02 22:47:46 +0300

    Add 1 lesson

---
+++ src/module1/lesson1/simple.py
@@ -0,0 +1,60 @@
+# Training
+# TODO: Вычислить сумму двух чисел
+def amount(a, b):
+    pass
+
+
+# Training
+# TODO: Вычислить разность двух чисел
+def subtraction(a, b):
+    pass
+
+
+# Training
+# TODO: Вычислить произведение двух чисел
+def multiplication(a, b):
+    pass
+
+
+# Training
+# TODO: Вычислить деление двух чисел
+def division(a, b):
+    pass
+
+
+# Training
+# TODO: Вычислить остаток от деления двух чисел
+def mod(a, b):
+    pass
+
+
+# Training
+# TODO: Вычислить целую часть от деления двух чисел
+def integer_part(a, b):
+    pass
+
+
+# Trivial
+# TODO: Вычислить дискриминант квадратного уравнения
+def discriminant(a, b, c):
+    pass
+
+
+# Trivial
+# TODO: Поиск одного из корней квадратного уравнения
+def sqrt(a, b, c):
+    pass
+
+
+# Trivial
+# Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
+# TODO: Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
+def seconds(hours, minutes, seconds):
+    pass
+
+
+# Easy
+# Пользователь задает целое трехзначное число (например, 478).
+# TODO: Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
+def revert_number(number):
+    pass
---
+++ tests/module1/lesson1/simple_tests.py
@@ -0,0 +1,53 @@
+import unittest
+
+from src.module1.lesson1 import simple
+
+
+class TestSimple(unittest.TestCase):
+
+    def test_amount(self):
+        self.assertEqual(10, simple.amount(5, 5))
+        self.assertEqual(-5, simple.amount(10, -15))
+
+    def test_subtraction(self):
+        self.assertEqual(5, simple.subtraction(10, 5))
+        self.assertEqual(15, simple.subtraction(10, -5))
+
+    def test_multiplication(self):
+        self.assertEqual(2, simple.multiplication(1, 2))
+        self.assertEqual(0, simple.multiplication(0, 2))
+        self.assertEqual(-2, simple.multiplication(-1, 2))
+
+    def test_division(self):
+        self.assertEqual(2.0, simple.division(2, 1))
+        self.assertEqual(-2.0, simple.division(8, -4))
+
+    def test_mod(self):
+        self.assertEqual(0, simple.mod(2, 1))
+        self.assertAlmostEqual(-3.7, simple.mod(8.3, -4), delta=1e-5)
+
+    def test_integer_part(self):
+        self.assertEqual(2.0, simple.integer_part(2, 1))
+        self.assertEqual(-2.0, simple.integer_part(8, -4))
+
+    def test_discriminant(self):
+        self.assertAlmostEqual(0.0, simple.discriminant(0.0, 0.0, 0.0), delta=1e-5)
+        self.assertAlmostEqual(0.0, simple.discriminant(1.0, -2.0, 1.0), delta=1e-5)
+        self.assertAlmostEqual(1.0, simple.discriminant(1.0, 3.0, 2.0), delta=1e-5)
+
+    def test_sqrt(self):
+        self.assertAlmostEqual(1.0, simple.sqrt(1.0, -2.0, 1.0), delta=1e-5)
+        self.assertAlmostEqual(-3.0, simple.sqrt(1.0, 6.0, 9.0), delta=1e-5)
+
+    def test_seconds(self):
+        self.assertEqual(30035, simple.seconds(8, 20, 35))
+        self.assertEqual(86400, simple.seconds(24, 0, 0))
+        self.assertEqual(13, simple.seconds(0, 0, 13))
+
+    def test_revert_number(self):
+        self.assertEqual(874, simple.revert_number(478))
+        self.assertEqual(201, simple.revert_number(102))
+
+
+if __name__ == '__main__':
+    unittest.main()
