5ca2be524104351eacc806b5f913747c30aa7843
lexcorp <lr.romanov1999@mail.ru>
2020-09-05 12:38:51 +0300

    Добавил дополнительные задания ко второму уроку

---
+++ src/module1/lesson1/example.py
@@ -0,0 +1,26 @@
+# НУЖНОЕ РАСКОММЕНТИРОВАТЬ
+
+# Из строки в число
+# num_str = "10000"
+# print(type(num_str))
+# num = int(num_str)
+# print(type(num))
+# print(num)
+
+# Из числа в строку
+# num = 100
+# print(type(num))
+# num_str = str(num)
+# print(type(num_str))
+
+# Из числа в дробное число
+# num = 100
+# print(type(num))
+# num_float = float(num)
+# print(type(num_float))
+# print(num_float)
+
+# number = input("Введите число ")
+# number_int = int(number)
+#
+# number = int(input("Введите число "))
---
+++ src/module1/lesson2/example.py
@@ -0,0 +1,32 @@
+# НУЖНОЕ РАСКОММЕНТИРОВАТЬ
+
+# true = True
+# false = False
+# print(true)
+# print(false)
+
+# a = 10
+# b = 0
+# c = -34545
+# bool_a = bool(a)
+# bool_b = bool(b)
+# bool_c = bool(c)
+# print(bool_a)  # True
+# print(bool_b)  # False
+# print(bool_c)  # True
+
+# a = "sdffdsf"
+# b = ""
+# bool_a = bool(a)
+# bool_b = bool(b)
+# print(bool_a)  # True
+# print(bool_b)  # False
+
+# true = True
+# false = False
+# print(int(true))  # 1
+# print(int(false))  # 0
+# print(str(true))  # True
+# print(str(false))  # False
+# print(float(true))  # 1.0
+# print(float(false))  # 0.0
