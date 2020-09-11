b32bf2f022687ca65de9cd6ef1c1fab86f8822a6
ba3atpoh <petrunino96@gmail.com>
2020-09-11 21:26:44 +0300

    Начали цикл вайл

--- src/module1/lesson3/while_tasks/task.py
+++ src/module1/lesson3/while_tasks/task.py
@@ -1,3 +1,12 @@
 # Цикл WHILE
 # Используя счётчик, напиши программу,
 # которая 5 раз выводит слово “Hello”.
+count = 0
+while count < 5:
+    print('Hello')
+    count = count + 1
+
+count = 5
+while count > 0:
+    print('Hello')
+    count = count - 1
