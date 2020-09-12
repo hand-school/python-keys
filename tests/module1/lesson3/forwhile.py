ffc4f8e9a66bfbf207a6fa7d9ef6b792c5d193bd
lexcorp <lr.romanov1999@mail.ru>
2020-09-12 20:53:48 +0300

    Обновил 3ий урок

--- tests/module1/lesson3/forwhile.py
+++ tests/module1/lesson3/forwhile.py
@@ -23,18 +23,18 @@ class TestTasks(unittest.TestCase):
         pass  # TODO: тесты написать самостоятельно
 
     def test_counter_for(self):
-        self.assertEqual(11, counter(1, 10, 1))
-        self.assertEqual(15, counter(1, 2, 7))
+        self.assertEqual(11, counter_for(1, 10, 1))
+        self.assertEqual(15, counter_for(1, 2, 7))
 
     def test_num_count_for(self):
-        self.assertEqual(1, num_count(1))
-        self.assertEqual(3, num_count(123))
-        self.assertEqual(10, num_count(1231231231))
+        self.assertEqual(1, num_count_for(1))
+        self.assertEqual(3, num_count_for(123))
+        self.assertEqual(10, num_count_for(1231231231))
 
     def test_custom_len_for(self):
-        self.assertEqual(3, custom_len("123"))
-        self.assertEqual(0, custom_len(""))
-        self.assertEqual(7, custom_len("1212123"))
+        self.assertEqual(3, custom_len_for("123"))
+        self.assertEqual(0, custom_len_for(""))
+        self.assertEqual(7, custom_len_for("1212123"))
 
     def test_revert_for(self):
         pass  # TODO: тесты написать самому
