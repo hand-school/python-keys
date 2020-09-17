2a295866696947be5a69bf0dea702b418c9c94a6
lexcorp <lr.romanov1999@mail.ru>
2020-09-17 19:10:24 +0300

    Обновил 4ий урок

---
+++ src/module1/lesson4/strings.py
@@ -0,0 +1,40 @@
+# Training
+# TODO: найти длину слова
+def use_len(string):
+    pass
+
+
+# Training
+# TODO: Функция получает на вход слово и выводит его первый и последний символ через “-”.
+# Пример: "Привет" -> "П-т"
+def cut_word(string):
+    pass
+
+
+# Training
+# TODO: Найти первое вхождения символа symbol в сообщение sms справа и слева
+# Результат вывести через запятую
+def r_l_find(sms, symbol):
+    pass
+
+
+# Training
+# TODO: заменить в строке string символы old_symbol на new_symbol
+def replace_chars(string, old_symbol, new_symbol):
+    pass
+
+
+# Training
+# TODO:
+def count_chars(sms, symbol):
+    pass
+
+
+# Easy
+# Используя методы find() и rfind(), напиши функцию,
+# которая находит индекс второго вхождения символа symbol
+# в неё и выводит его на экран. Если символ встречается один раз,
+# программа должна вернуть -1, а если ни одного — вернуть -2.
+# ТЕСТЫ НАПИСАТЬ САМОСТОЯТЕЛЬНО
+def second_symbol(word, symbol):
+    pass
