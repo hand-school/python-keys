a8a291ebaff0acfee7dfbf84ee6b23dd247837d7
lexcorp <lr.romanov1999@mail.ru>
2020-10-12 21:53:08 +0300

    Удалил решения

--- src/module1/lesson1/simple.py
+++ src/module1/lesson1/simple.py
@@ -1,69 +1,57 @@
 # Training
 # TODO: Вычислить сумму двух чисел
 def amount(a, b):
-    c=a+b
-    return c
+    pass
 
 
 # Training
 # TODO: Вычислить разность двух чисел
 def subtraction(a, b):
-    c=a-b
-    return c
+    pass
 
 
 # Training
 # TODO: Вычислить произведение двух чисел
 def multiplication(a, b):
-    c=a*b
-    return c
+    pass
 
 
 # Training
 # TODO: Вычислить деление двух чисел
 def division(a, b):
-    c=a/b
-    return c
+    pass
 
 
 
 # Training
 # TODO: Вычислить остаток от деления двух чисел
 def mod(a, b):
-    c=a%b
-    return c
+    pass
 
 
 # Training
 # TODO: Вычислить целую часть от деления двух чисел
 def integer_part(a, b):
-    c=a//b
-    return c
+    pass
 
 
 # Trivial
 # TODO: Вычислить дискриминант квадратного уравнения
 def discriminant(a, b, c):
-    d=b**2-4*a*c
-    return d
+    pass
 
 
 # Trivial
 # TODO: Поиск одного из корней квадратного уравнения
 def sqrt(a, b, c):
-    x=discriminant(a,b,c)
-    result=(-b+x**1/2)/(2*a)
-    return result
+    pass
 
 
 # Trivial
 # Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
 # TODO: Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
 def seconds(hours, minutes, seconds):
-    h = hours * 3600
-    m = minutes * 60
-    s = 35
-    time = h + m + s
+    pass
 
 
 # Trivial
