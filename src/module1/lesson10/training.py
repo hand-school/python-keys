041822334d609a1a026a26522cfebbfedac9d2b0
ba3atpoh <petrunino96@gmail.com>
2020-10-26 00:48:41 +0300

    начал создание ПВП

--- src/module1/lesson10/training.py
+++ src/module1/lesson10/training.py
@@ -1,28 +1,26 @@
 import random
 
 
-class Characters():
-    def __init__(self, name, dmg, max_hp=100):
+class Characters:
+    def __init__(self, name, dmg, defence, max_hp):
         self.name = name
         self.dmg = dmg
         self.hp = max_hp
+        self.defence = defence
 
+    def hit(self, character):
+        character.hp -= (self.dmg - character.defence) * random.uniform(0.75, 1.15)
+        print(character.name + ' пизданул по ' + character.name)
+
+    def miss(self, character):
+        character.hp -= (self.dmg - character.defence) * random.uniform(0.75, 1.15)
 
 
 class Human(Characters):
 
-
-    def __init__(self, name, dmg, max_hp=100, defence=0, mana=100):
-        super.__init__(self,name, dmg, max_hp=100)
-        self.name = name
-        self.dmg = dmg
-        self.hp = max_hp
+    def __init__(self, name='Olegator', dmg=3, max_hp=100, defence=0, mana=100):
+        super().__init__(name, dmg, defence, max_hp)
         self.mana = mana
-        self.defence = defence
-
-    def hit(self, character):
-        character.hp -= (self.dmg - self.defence) * random.uniform(0.75, 1.15)
-        print(character.name + ' пизданул по ' + character.name)
 
     def buff(self, heal):
         if self.mana >= 25:
@@ -30,15 +28,7 @@ class Human(Characters):
             self.mana -= 25
             print(' ~Господи, похилимся!!~ ')
         else:
-            print(' ~Маны нет!~ ')
-
-    def use_hp(self):
-        if self.heal_potion > 0:
-            self.hp += 50
-            self.heal_potion -= 1
-            print(self.hp)
-        else:
-            print(' Ой... Нет хилок! ')
+            print(' ~Ой... Маны нет!~ ')
 
     def info(self):
         print()
@@ -46,11 +36,6 @@ class Human(Characters):
 
 class Ogre(Characters):
 
-    def __init__(self, name, dmg, max_hp=100, defence=3):
-        super().__init__(name, name, dmg, max_hp=100)
-        self.name = name
-        self.dmg = dmg
-        self.hp = max_hp
-        self.defence = defence
-
+    def __init__(self, name='AlexMeister', dmg=4, defence=3, max_hp=100):
+        super().__init__(name, dmg, defence, max_hp)
 
