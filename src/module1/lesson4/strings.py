b1e4d544e2a8fa3808c0e97b631b15c8f47f4b14
expert40rus <vladik.monstor@mail.ru>
2020-11-06 17:21:40 +0300

    Сделал 4 урок, cut world не понял

--- src/module1/lesson4/strings.py
+++ src/module1/lesson4/strings.py
@@ -1,33 +1,33 @@
 # Training
 # TODO: найти длину слова
 def use_len(string):
-    pass
+    return len(string)
 
 
 # Training
 # TODO: Функция получает на вход слово и выводит его первый и последний символ через “-”.
 # Пример: "Привет" -> "П-т"
 def cut_word(string):
-    pass
+    return string.replace(string[1:-1], "-")
 
 
 # Training
 # TODO: Найти первое вхождения символа symbol в сообщение sms справа и слева
 # Результат вывести через запятую
 def r_l_find(sms, symbol):
-    pass
+    return sms.find(symbol), sms.rfind(symbol)
 
 
 # Training
 # TODO: заменить в строке string символы old_symbol на new_symbol
 def replace_chars(string, old_symbol, new_symbol):
-    pass
+    return string.replace(old_symbol, new_symbol)
 
 
 # Training
 # TODO: Вернуть количетсов вхождений символа в строку
 def count_chars(sms, symbol):
-    pass
+    return sms.count(symbol)
 
 
 # Easy
@@ -37,4 +37,10 @@ def count_chars(sms, symbol):
 # программа должна вернуть -1, а если ни одного — вернуть -2.
 # ТЕСТЫ НАПИСАТЬ САМОСТОЯТЕЛЬНО
 def second_symbol(word, symbol):
-    pass
+    if word.find(symbol) == -1:
+        return -2
+    elif word.find(symbol) == word.rfind(symbol):
+        return -1
+    else:
+        i = word[word.find(symbol) + 1:word.rfind(symbol) + 1]
+        return i.find(symbol) + word.find(symbol) + 1
