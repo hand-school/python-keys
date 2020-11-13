ce9ddd9032ba4e71009edd011161c33874be1704
lexcorp <lr.romanov1999@mail.ru>
2020-11-13 22:24:57 +0300

    Примеры сортировки списков

--- src/module1/lesson6/example.py
+++ src/module1/lesson6/example.py
@@ -1,3 +1,17 @@
+# list1 = [0, 1, 2, 3, 4, 45, 32, 234, 5, 6, 7]
+#
+# sorted_list = sorted(list1)
+# print(sorted_list)
+#
+# reversed_sorted_list = sorted(list1, reverse=True)
+# print(reversed_sorted_list)
+#
+# list1.sort()
+# print(list1)
+#
+# list1.sort(reverse=True)
+# print(list1)
+
 def sort_with_opt(list1):
     is_changed = False
     check_iter = 0
