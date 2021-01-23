fd05c491f01d9ebaa66b1b461f12e3e041d52351
lexcorp <lr.romanov1999@mail.ru>
2021-01-23 21:43:37 +0300

    Урок 15: Многопоточное программирование

--- src/module1/lesson14/logging.py
+++ src/module1/lesson14/logging.py
@@ -1,20 +1,23 @@
+class Level:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
 class Logger:
-    class Level:
-        ERROR = 1
-        WARNING = 2
-        DEBUG = 3
-        INFO = 4
-
-    Level = Level()
-
-    levels = ("ERROR", "WARNING", "DEBUG", "INFO")
+    ERROR = Level("ERROR", 1)
+    WARNING = Level("WARNING", 2)
+    DEBUG = Level("DEBUG", 3)
+    INFO = Level("INFO", 4)
 
     def __init__(self, logger_name):
+        print("init")
         self.logger_name = logger_name
 
-    def log(self, level, msg):
-        log_msg = f'<{self.logger_name}> [{self.levels[level - 1]}]: {msg}'
+    def log(self, level: Level, msg):
+        log_msg = f'<{self.logger_name}> [{level}]: {msg}'
         print(log_msg)
 
     def info(self, msg):
-        self.log(self.Level.INFO, msg)
+        self.log(Logger.INFO, msg)
+
+Logger("name")
