a9c32dc5fea00e5c5547b780167cd541de467ea6
procenchec <procenchec@gmail.com>
2021-02-09 18:12:20 +0300

    Отредактировал weather.py

--- src/bot.py
+++ src/bot.py
@@ -4,15 +4,13 @@
 
 from time import sleep
 
-import config
+import src.config
+import src.weather
 import telebot
 import requests
 
 
-url = 'http://api.openweathermap.org/data/2.5/weather'
-bot = telebot.TeleBot(config.token)
 sleep(1)
-
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -26,37 +24,7 @@ def assist(message):
                      '/start запуск бота\n/help команды бота\nчтобы узнать погоду напишите в чат название города')
 
 
-@bot.message_handler(content_types=['text'])
-def city_weather(message):
-    city_name = message.text
-
-    try:
-        params = {'APPID': config.api_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
-        result = requests.get(url, params=params)
-        weather = result.json()
-
-        if weather["main"]['temp'] < 10:
-            status = "Сейчас холодно!"
-        elif weather["main"]['temp'] < 15:
-            status = "Сейчас прохладно!"
-        elif weather["main"]['temp'] > 30:
-            status = "Сейчас жарко!"
-        else:
-            status = "Сейчас отличная температура!"
-
-        bot.send_message(message.chat.id, f"Текущая температура в городе " + str(weather["name"]) + ': ' + str(
-            float(weather["main"]['temp'])) + "°С. Ощущается как: " + str(float(weather['main']['feels_like'])) + "\n" +
-                         "Максимальная температура: " + str(float(weather['main']['temp_max'])) + "°С\n" +
-                         "Минимальная температура: " + str(float(weather['main']['temp_min'])) + "°С\n" +
-                         "Скорость ветра: " + str(float(weather['wind']['speed'])) + ' м/с' + "\n" +
-                         "Давление: " + str(int(weather['main']['pressure']*0.750062)) + ' мм. рт. ст.' + "\n" +
-                         "Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" +
-                         "Видимость: " + str(weather['visibility']) + " м\n" +
-                         "Описание: " + str(weather['weather'][0]["description"])
-                         + "\n\n" + status)
-
-    except:
-        bot.send_message(message.chat.id, "Город " + city_name + " не найден")
+# Тут метод weather
 
 
 if __name__ == '__main__':
