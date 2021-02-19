0f22cc71cc9f24e14f61ce8f1f6bf441cc8cb7c4
TarakanovNI@studklg.ru <vNPGVfUWAY7Mgdg>
2021-02-19 20:50:55 +0300

    lessons 17 TCP/IP Get, post, delete

--- src/module1/lesson17/examples.py
+++ src/module1/lesson17/examples.py
@@ -1,12 +1,21 @@
 import requests
 
-# r = requests.get('http://localhost:5000/todo/api/v1.0/tasks')
-# print(r.status_code)
-# print(r.headers)
-# print(r.text)
 
-r = requests.get('http://localhost:5000/todo/api/v1.0/tasks/2')
+r = requests.get('http://localhost:5000/todo/api/v1.0/tasks')
+
 print(r.status_code)
 print(r.headers)
 print(r.text)
+
+r = requests.post('http://localhost:5000/todo/api/v1.0/tasks',
+                  data={"title":"Neponiatnaya xuyta","description":"polnaya xuynia","done":True})
+# r = requests.put('http://localhost:5000/todo/api/v1.0/tasks/1',data={"title":"Hi, my Dick","description":"Flax for Py - govno","done":True})
+print(r.status_code)
+print(r.headers)
+print(r.text)
+r = requests.get('http://localhost:5000/todo/api/v1.0/tasks/3')
+print(r.status_code)
+print(r.headers)
+print(r.text)
+r = requests.delete('http://localhost:5000/todo/api/v1.0/tasks/3')
 
