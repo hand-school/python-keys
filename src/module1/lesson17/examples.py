2367be3b2845c33f4a8e4a18f2c9660bca6c7718
Vlad <vladik.monstor@mail.ru>
2021-02-19 21:00:13 +0300

    сделал post и делит

--- src/module1/lesson17/examples.py
+++ src/module1/lesson17/examples.py
@@ -10,3 +10,23 @@ print(r.status_code)
 print(r.headers)
 print(r.text)
 
+r = requests.put('http://localhost:5000/todo/api/v1.0/tasks/2', data={'id': 1,
+                                                                    'title': u'Buy groceries',
+                                                                    'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+                                                                    'done': False})
+print(r.status_code)
+print(r.headers)
+print(r.text)
+
+# r = requests.post('http://localhost:5000/todo/api/v1.0/tasks', data={'id': 1,
+#                                                                     'title': u'Buy groceries',
+#                                                                     'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+#                                                                     'done': False})
+# print(r.status_code)
+# print(r.headers)
+# print(r.text)
+
+# r = requests.delete('http://localhost:5000/todo/api/v1.0/tasks/2')
+# print(r.status_code)
+# print(r.headers)
+# print(r.text)
