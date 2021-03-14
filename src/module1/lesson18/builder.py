207d3ea2cfed175454921f4105bb6042f2891759
lexcorp <lr.romanov1999@mail.ru>
2021-03-14 19:03:25 +0300

    lesson 18 update Builder example

--- src/module1/lesson18/builder.py
+++ src/module1/lesson18/builder.py
@@ -27,4 +27,23 @@ class House:
         return self
 
 
+# Пример 0
+home0 = House(100, 100, 100, ["M1", "M2"])
+
+# Пример 1
 home1 = House().set_width(100).set_height(100).set_materials(["M1", "M2"]).set_length(100)
+
+# Пример 2
+home2 = House()
+print(home2.__dict__)
+
+width = int(input("Введите ширину "))
+home2.set_width(width).set_materials(["M1", "M2"])
+print(home2.__dict__)
+
+length = int(input("Введите длину "))
+home2.set_length(length)
+print(home2.__dict__)
+
+home2.set_height(int(input("Введите высоту ")))
+print(home2.__dict__)
