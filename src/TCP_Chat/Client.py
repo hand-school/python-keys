7fc0db8e02e311c2e80b78ad7311489084579f1c
procenchec <procenchec@gmail.com>
2021-03-14 17:57:28 +0300

    TCP Chat localhost utf-32

--- src/TCP_Chat/Client.py
+++ src/TCP_Chat/Client.py
@@ -2,41 +2,33 @@ import socket
 import threading
 
 
-# Choosing Nickname
 nickname = input("Choose your nickname: ")
 
-# Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55555))
+client.connect(('localhost', 2233))
 
 
-# Listening to Server and Sending Nickname
 def receive():
     while True:
         try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-32')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-32'))
             else:
                 print(message)
         except:
-            # Close Connection When Error
             print("An error occured!")
             client.close()
             break
 
 
 
-# Sending Messages To Server
 def write():
     while True:
         message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        client.send(message.encode('utf-32'))
 
 
-# Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
