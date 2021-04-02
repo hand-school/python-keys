345477cd61de5519a6474b4c739763ebd1bacd6a
Vlad <vladik.monstor@mail.ru>
2021-04-02 17:38:08 +0300

    Выдача фильма по жанру с Кинопоиска

--- src/kinopoisk.py
+++ src/kinopoisk.py
@@ -4,6 +4,7 @@ import random
 kinopoisk = KP(token='9736285b-b7d0-4a40-9f9e-7cfb09354984')
 
 
+
 def get_cinema(genre):
     search_result = kinopoisk.search(genre)
     return search_result[random.randrange(0, 5)].ru_name
